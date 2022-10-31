7641be8b7436d3c6f57389136707b805bb9d4460
Joseff361 <Joseff361@outlook.com>
2022-10-30 21:55:10 -0500

    Finishing refresher videos

--- Section 19 - Advanced Redux/src/components/Cart/CartItem.js
+++ Section 19 - Advanced Redux/src/components/Cart/CartItem.js
@@ -1,7 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
+
 import classes from './CartItem.module.css';
 
-const CartItem = (props) => {
+const CartItem = props => {
+  const dispatch = useDispatch();
+
   const { title, quantity, total, price } = props.item;
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItem(props.id));
+  };
+
+  const addItemHandler = () => {
+    dispatch(cartActions.addItem({ id: props.id, title, price }));
+  };
 
   return (
     <li className={classes.item}>
@@ -17,8 +30,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
